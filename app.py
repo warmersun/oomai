8117b6091612e024b2a82029d48f06cc85795baa
Sic <sic@warmersun.com>
2025-11-22 19:17:34 -0800

    split out auth

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ from license_management import (
     get_paid_amount_left,
     upsert_client_reference_id,
 )
-
+import auth
 
 from config import OPENAI_API_KEY, GROQ_API_KEY, XAI_API_KEY, ELEVENLABS_API_KEY, ELEVENLABS_VOICE_ID, NEO4J_URI, NEO4J_USERNAME, NEO4J_PASSWORD, DESCOPE_PROJECT_ID 
 
@@ -442,51 +442,6 @@ async def on_message(message: cl.Message):
             await step.remove()
 
         cl.user_session.set("last_message", output_message.content)
-
-
-@cl.password_auth_callback
-def auth_callback(username: str, password: str) -> Optional[cl.User]:
-    if (username, password) == ("Sic", "kadima"):
-        return cl.User(identifier="Sic",
-                       metadata={
-                           "role": "admin",
-                           "provider": "credentials"
-                       })
-    elif (username, password) == ("User", "oom.today"):
-        return cl.User(identifier=username,
-                       metadata={
-                           "role": "user",
-                           "provider": "credentials"
-                       })
-    else:
-        return None
-
-
-# @cl.oauth_callback
-# def oauth_callback(
-#     provider_id: str,
-#     token: str,
-#     raw_user_data: Dict[str, str],
-#     default_user: cl.User,
-# ) -> Optional[cl.User]:
-    logger.info(f"OAuth callback: {provider_id}, {token}, {raw_user_data}")
-    assert DESCOPE_PROJECT_ID is not None, "DESCOPE_PROJECT_ID is not set"
-    descope_client = DescopeClient(project_id=DESCOPE_PROJECT_ID)
-    roles = [
-        "admin",
-    ]
-    try:
-        jwt_response = descope_client.validate_session(
-            session_token=token, audience=DESCOPE_PROJECT_ID)
-        is_admin_role = descope_client.validate_roles(jwt_response, roles)
-        logger.info(f"Is admin role?: {is_admin_role}")
-        if is_admin_role:
-            default_user.metadata["role"] = "admin"
-    except Exception as error:
-        logger.error(f"Error getting matched roles: {error}")
-    finally:
-        return default_user
-
 
 # Text to Speech
 
---
+++ auth.py
@@ -0,0 +1,45 @@
+import chainlit as cl
+from typing import Optional
+
+
+@cl.password_auth_callback
+def auth_callback(username: str, password: str) -> Optional[cl.User]:
+    if (username, password) == ("Sic", "kadima"):
+        return cl.User(identifier="Sic",
+                       metadata={
+                           "role": "admin",
+                           "provider": "credentials"
+                       })
+    elif (username, password) == ("User", "oom.today"):
+        return cl.User(identifier=username,
+                       metadata={
+                           "role": "user",
+                           "provider": "credentials"
+                       })
+    else:
+        return None
+
+# @cl.oauth_callback
+# def oauth_callback(
+#     provider_id: str,
+#     token: str,
+#     raw_user_data: Dict[str, str],
+#     default_user: cl.User,
+# ) -> Optional[cl.User]:
+#     logger.info(f"OAuth callback: {provider_id}, {token}, {raw_user_data}")
+#     assert DESCOPE_PROJECT_ID is not None, "DESCOPE_PROJECT_ID is not set"
+#     descope_client = DescopeClient(project_id=DESCOPE_PROJECT_ID)
+#     roles = [
+#         "admin",
+#     ]
+#     try:
+#         jwt_response = descope_client.validate_session(
+#             session_token=token, audience=DESCOPE_PROJECT_ID)
+#         is_admin_role = descope_client.validate_roles(jwt_response, roles)
+#         logger.info(f"Is admin role?: {is_admin_role}")
+#         if is_admin_role:
+#             default_user.metadata["role"] = "admin"
+#     except Exception as error:
+#         logger.error(f"Error getting matched roles: {error}")
+#     finally:
+#         return default_user
