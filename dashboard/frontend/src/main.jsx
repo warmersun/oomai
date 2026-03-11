86abdd4b312fd83264001aca0eaac17469f5a0d0
Warmer Sun <tamas.simon@warmersun.com>
2026-03-10 23:18:59 -0700

    Simplify Chainlit theme to solid dashboard colors

--- dashboard/frontend/src/main.jsx
+++ dashboard/frontend/src/main.jsx
@@ -1,22 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RecoilRoot } from 'recoil';
-import { ChainlitAPI, ChainlitContext } from '@chainlit/react-client';
 import App from './App';
 import './index.css';
-
-const defaultChainlitServer = import.meta.env.DEV
-    ? `${window.location.origin}/chainlit`
-    : `${window.location.protocol}//${window.location.hostname}:8000`;
-const CHAINLIT_SERVER = import.meta.env.VITE_CHAINLIT_SERVER || defaultChainlitServer;
-const apiClient = new ChainlitAPI(CHAINLIT_SERVER, 'webapp');
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <RecoilRoot>
-            <ChainlitContext.Provider value={apiClient}>
-                <App />
-            </ChainlitContext.Provider>
+            <App />
         </RecoilRoot>
     </React.StrictMode>
 );
